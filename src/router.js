a225406a0d44395dc90f141c190b26c72d216d1f
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-06-16 17:05:03 +0200

    feat: add routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+import AppHome from './pages/AppHome.vue';
+import RestaurantIndex from './pages/RestaurantIndex.vue';
+
 const router = createRouter({
     history: createWebHistory(),
 
     routes: [
-
+        {
+            path: '/',
+            name: 'home',
+            component: AppHome,
+        },
+        {
+            path: '/restaurant',
+            name: 'restaurant',
+            component: RestaurantIndex,
+        },
     ]
 });
 
