8632a09201ad6f4db98a0a26ec00c26ad5a18df2
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-19 12:14:28 +0200

    feat: add restaurant show with dishes

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import AppHome from './pages/AppHome.vue';
 import RestaurantIndex from './pages/RestaurantIndex.vue';
+import RestaurantShow from './pages/RestaurantShow.vue';
+
 
 const router = createRouter({
     history: createWebHistory(),
@@ -11,13 +13,27 @@ const router = createRouter({
             path: '/',
             name: 'home',
             component: AppHome,
+            meta: {title: 'Home'},
         },
         {
             path: '/restaurant',
             name: 'restaurant',
             component: RestaurantIndex,
+            meta: {title: 'Restaurants'},
+        },
+        {
+            path: '/restaurantMenu/:slug',
+            name: 'restaurantMenu',
+            component: RestaurantShow,
+            meta: {title: 'Restaurant menu'},
         },
     ]
+});
+
+router.beforeEach((to, from) => {
+
+    document.title = to.meta?.title ?? 'Deliveboo'
+
 });
 
 export { router };
