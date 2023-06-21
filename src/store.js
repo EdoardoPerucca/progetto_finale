4b65c156c0d8332ebdaef648b09b9a19ffce6c69
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-21 18:30:30 +0200

    feat: add localStorage variables

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import {reactive} from "vue";
 
 export const store = reactive({
     
-    cart: [],
     cartFromLocalStorage: JSON.parse(localStorage.getItem('cart') || '[]'),
 
-    total: 0,
-    totalFromLocalStorage: JSON.parse(localStorage.getItem('total') || 0),
+    totalFromLocalStorage: JSON.parse(localStorage.getItem('total') || '0'),
+
+    restaurantIdFromLocalStorage: JSON.parse(localStorage.getItem('id') || 'null'),
 
     dishIds: [],
+
+    actualRestaurantId: null,
 
 });
