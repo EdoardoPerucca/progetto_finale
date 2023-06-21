0bc6f4c39ff3f4418f921f22b25844b15d2ef740
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-21 12:05:13 +0200

    feat: add Cart component

---
+++ src/pages/Cart.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: 'Cart',
+
+    data() {
+        return {
+            
+        }
+    },
+}
+</script>
+
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+    
+    
+</style>
