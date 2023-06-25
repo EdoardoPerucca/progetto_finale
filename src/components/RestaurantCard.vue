275ff0740ea11db1355e2ab22587128b18fbc3a2
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-25 16:01:40 +0200

    feat: fix code

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -12,15 +12,20 @@ export default {
 
     methods: {
         getImage(){
-           let  path = 'https://static.vecteezy.com/ti/vettori-gratis/p1/5359703-cibo-icone-pixel-perfetto-illustrazione-vettoriale.jpg'
+
+            let  path = 'https://static.vecteezy.com/ti/vettori-gratis/p1/5359703-cibo-icone-pixel-perfetto-illustrazione-vettoriale.jpg';
+
             if (this.restaurant.cover_image == path) {
-                return path
+
+                return path;
+
             } else {
+
                 // console.log(this.restaurant.cover_image)
-                return 'http://127.0.0.1:8000/storage/' + this.restaurant.cover_image
+                return 'http://127.0.0.1:8000/storage/' + this.restaurant.cover_image;
                 
-            }
-        }
+            };
+        },
     },
 }
 
@@ -29,33 +34,22 @@ export default {
 
 <template>
 
-    
-
     <div class="card py-3" style="width:calc(80% / 3 )">
-
         <img :src="getImage()" class="card-img-top" alt="...">
-
         <div class="card-body">
-
             <h5 class="card-title">{{ restaurant.activity_name }}</h5>
             <ul class="list-group list-group-flush">
-
                 <li class="list-group-item"><b>Via: </b>{{ restaurant.address }}</li>
                 <li class="list-group-item"><b>Tel: </b>{{ restaurant.phone_number }}</li>
                 <li class="list-group-item"><b>Tipologia: </b>
                     <span  v-for="restaurantType in restaurant.types">
                         {{ restaurantType.name + ', ' }}
                     </span>
-                </li>
-                
+                </li>     
             </ul>
-
             <router-link class="btn btn-primary" :to="{name: 'restaurantMenu', params: {slug: restaurant.slug}}">Visualizza menu</router-link>
-
         </div>
     </div>
-
-    
     
 </template>
 
