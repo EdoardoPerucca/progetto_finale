ddae5a682df33986b12eece6235dd7b3cd77f8ea
Aranel9966 <99angelo.dire@gmail.com>
2023-06-28 18:06:46 +0200

    fest:add style cart

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -111,70 +111,212 @@ export default {
 
 
 <template>
-
-    <router-link class="btn btn-primary m-3" :to="{name: 'restaurant'}">Torna ai Ristoranti</router-link>
-
+    <div class="bg">
+        <router-link class="btn btn-rest m-3" :to="{name: 'restaurant'}">Torna ai Ristoranti</router-link>
     
-    <h2 class="text-center">
-        {{ this.store.restaurantNameFromLocalStorage }}
-    </h2>
-
-
-    <div  class="container flex justify-content-center">
-
-        <form class="mt-5" @submit.prevent="pushIntoOrder">
-            <div class="mb-3">
-                <label for="first_name" class="form-label">*Nome</label>
-                <input v-model="store.first_name" type="text" class="form-control" id="first_name" aria-describedby="emailHelp" required>
-            </div>
-            <div class="mb-3">
-                <label for="last_name" class="form-label">*Cognome:</label>
-                <input v-model="store.last_name" type="text" class="form-control" id="last_name" required>
-            </div>
-            <div class="mb-3">
-                <label for="address" class="form-label">*Indirizzo:</label>
-                <input v-model="store.address" type="text" class="form-control" id="address" required>
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">*Email</label>
-                <input v-model="store.email" type="email" class="form-control" id="email" aria-describedby="emailHelp" required>
-                <div id="emailHelp" class="form-text">Non condivideremo mai la tua mail con altre persone.</div>
-            </div>
-            <table class="table">
-                <thead class="text-center">
-                    <tr>
-                        <th scope="col">Nome</th>
-                        <th scope="col">Prezzo</th>
-                        <th scope="col">Quantità</th>
-                    </tr>
-                </thead>
-                <tbody class="text-center">
-                    <tr  v-for="(dish, index) in this.store.cartFromLocalStorage" :key="index">
-                        <td>{{ dish.name }}</td>
-                        <td>€ {{dish.price.toFixed(2)}}</td>
-                        <td >{{ dish.quantity }}</td>
-                    </tr>
-                    <tr>
-                        <th>Totale</th>
-                        <td v-if="this.store.cartFromLocalStorage.length == 0 || this.store.restaurantIdFromLocalStorage == this.store.cartFromLocalStorage[0].restaurant_id || this.store.actualRestaurantId == this.store.cartFromLocalStorage[0].restaurant_id">€ {{ this.store.totalFromLocalStorage.toFixed(2) }}</td>
-                        <td v-else>€ 0.00</td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
-
-            <div class="text-center">
-                <button type="submit" class="btn btn-primary">Procedi all'ordine</button>
-                <router-link class="ms-2 btn btn-danger" :to="{name: 'restaurant'}" @click="clearCart()">Elimina Carrello</router-link>
-            </div>
-        </form>
+        <div  class="delivery-form">
+            <h3 class="text-center">
+                {{ this.store.restaurantNameFromLocalStorage }}
+            </h3>
+            <!-- <h3 class="text-center">Inserisci i dati per la consegna</h3> -->
+    
+            <form class="mt-3" @submit.prevent="pushIntoOrder">
+                <div class="orm-group mt-2">
+                    <label for="first_name" class="form-label">*Nome</label>
+                    <input v-model="store.first_name" type="text" class="form-control" id="first_name" aria-describedby="emailHelp" required>
+                </div>
+                <div class="orm-group mt-2">
+                    <label for="last_name" class="form-label">*Cognome:</label>
+                    <input v-model="store.last_name" type="text" class="form-control" id="last_name" required>
+                </div>
+                <div class="orm-group mt-2">
+                    <label for="address" class="form-label">*Indirizzo:</label>
+                    <input v-model="store.address" type="text" class="form-control" id="address" required>
+                </div>
+                <div class="orm-group mt-2">
+                    <label for="email" class="form-label">*Email</label>
+                    <input v-model="store.email" type="email" class="form-control" id="email" aria-describedby="emailHelp" required>
+                    <div id="emailHelp" class="form-text"> <em> Non condivideremo mai la tua mail con altre persone.</em></div>
+                </div>
+                <table class="delivery-table">
+                    <thead class="text-center">
+                        <tr>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Prezzo</th>
+                            <th scope="col">Quantità</th>
+                        </tr>
+                    </thead>
+                    <tbody class="text-center">
+                        <tr  v-for="(dish, index) in this.store.cartFromLocalStorage" :key="index">
+                            <td>{{ dish.name }}</td>
+                            <td>€ {{dish.price.toFixed(2)}}</td>
+                            <td >{{ dish.quantity }}</td>
+                        </tr>
+                        <tr>
+                            <th>Totale</th>
+                            <td v-if="this.store.cartFromLocalStorage.length == 0 || this.store.restaurantIdFromLocalStorage == this.store.cartFromLocalStorage[0].restaurant_id || this.store.actualRestaurantId == this.store.cartFromLocalStorage[0].restaurant_id">€ {{ this.store.totalFromLocalStorage.toFixed(2) }}</td>
+                            <td v-else>€ 0.00</td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+    
+                <div class="text-center">
+                    <button type="submit" class="btn btn-restaurant">Procedi all'ordine</button>
+                    <router-link class="ms-2 btn btn-restaurant2" :to="{name: 'restaurant'}" @click="clearCart()">Elimina Carrello</router-link>
+                </div>
+            </form>
+    
+        </div>
 
     </div>
+
+
+
 
 </template>
 
 
 <style lang="scss" scoped>
+
+.bg{
+    background-image: url('https://i.ibb.co/fM5MH76/pngegg.png');
+    background-color: #ffcc6a;
+    padding-bottom: 15px;
+    
+}
+.btn-restaurant {
+    border: none;
+    border-radius: 50px;
+    background-color: #ffcc6a;
+    color: white;
+    font-weight: bold;
+    border-bottom: 3px solid #dd3f3f;
+    transition: all .6s;
+    box-shadow: 0px 4px 8px 0px rgb(0, 0, 0);
+
+    &:hover {
+        cursor: pointer;
+        background-color: rgb(136, 194, 48);
+        border-bottom: 3px solid #dd3f3f;
+    }
+}
+.btn-restaurant2 {
+    border: none;
+    border-radius: 50px;
+    background-color: #ffcc6a;
+    color: white;
+    font-weight: bold;
+    border-bottom: 3px solid #dd3f3f;
+    transition: all .6s;
+    box-shadow: 0px 4px 8px 0px rgb(0, 0, 0);
+
+    &:hover {
+        cursor: pointer;
+        background-color: #dd3f3f;
+        border-bottom: 3px solid #212529;
+    }
+}
+
+.delivery-table {
+  width: 100%;
+  border-collapse: collapse;
+  border-radius: 8px;
+  overflow: hidden;
+  margin: 20px 0;
+}
+
+
+.delivery-table th,.delivery-table td {
+    border-bottom: solid 1px #dd3f3f;
+    padding: 4px;
+  text-align: center;
+}
+
+.delivery-table thead {
+  background-color: #ffcc6a;
+  color: #fff;
+}
+
+em{
+    color: white;
+}
+
+.delivery-table tbody tr:hover {
+  background-color: #ffcc6a;
+
+}
+
+.delivery-table th:first-child,
+.delivery-table td:first-child {
+  border-radius: 8px 0 0 8px;
+}
+
+.delivery-table th:last-child,
+.delivery-table td:last-child {
+  border-radius: 0 8px 8px 0;
+}
+
+/////
+.delivery-form {
+    color: white;
+  max-width: 400px;
+  margin: 15px auto;
+  padding: 20px;
+  background-color: #212529;
+  border-radius: 15%;
+
+    border-top: 15px solid #dd3f3f;
+    border-bottom: 15px solid #dd3f3f;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.delivery-form h2 {
+  text-align: center;
+  margin-bottom: 20px;
+}
+
+.form-group {
+  margin-bottom: 20px;
+}
+
+.form-group label {
+  display: block;
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.form-group input,
+.form-group textarea {
+  width: 100%;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+}
+
+.form-group textarea {
+  resize: vertical;
+}
+
+.form-group button {
+  display: block;
+  width: 100%;
+  padding: 12px;
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  color: #fff;
+  background-color: #ff5f00;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.form-group button:hover {
+  background-color: #e65000;
+}
+
     
     .flex {
         display: flex;
@@ -185,5 +327,22 @@ export default {
         }
 
     }
+
+    .btn-rest {
+    border: none;
+    border-radius: 50px;
+    background-color: #212529;
+    color: white;
+    font-weight: bold;
+    border-bottom: 3px solid #dd3f3f;
+    transition: all .6s;
+    box-shadow: 0px 4px 8px 0px rgb(0, 0, 0);
+
+    &:hover {
+        cursor: pointer;
+        background-color: #dd3f3f;
+        border-bottom: 3px solid #212529;
+    }
+}
     
 </style>
