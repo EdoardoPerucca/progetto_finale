d99a8dc0e89ff51e5851b0f902c85596e3560f48
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-06-29 16:06:51 +0200

    feat: add style

--- src/pages/AboutUs.vue
+++ src/pages/AboutUs.vue
@@ -12,7 +12,7 @@ export default {
 }
 </script>
 <template>
-    <div>
+    <div class="all-page">
         <section class="about-us">
   <div class="container">
     <h2>About Us</h2>
@@ -22,12 +22,32 @@ export default {
     <p class="delivery-service">Unisciti a noi nella scoperta di un mondo di sapori, dove la qualità, la convenienza e la comodità si fondono armoniosamente. Scegli Delivero e vivi ogni pasto come un'esperienza culinaria senza confini.</p>
     <div class="app-download">
         <h2>Il Team</h2>
-    <ul>
+    <ul class="d-flex gap-3 fw-bold flex-wrap p-0">
         <li class="m-1">
             Angelo Di re 
             <a href="https://github.com/Aranel9966"><i class="fa-brands fa-github"></i></a>
         </li>
-     
+        <li class="m-1">
+            Francesco Falanga
+            <a href="https://github.com/FrancescoThomasFalanga"><i class="fa-brands fa-github"></i></a>
+        </li>
+        <li class="m-1">
+            Domenico Lucarelli
+            <a href="https://github.com/DomenicoLucarelli"><i class="fa-brands fa-github"></i></a>
+        </li>
+        <li class="m-1">
+            Marco Cocco
+            <a href="https://github.com/MarcoCocco"><i class="fa-brands fa-github"></i></a>
+        </li>
+        <li class="m-1">
+            Edoardo Perrucca
+            <a href="https://github.com/EdoardoPerucca"><i class="fa-brands fa-github"></i></a>
+        </li>
+        <li class="m-1">
+            Andrea Pomati
+            <a href="https://github.com/Andrea-Pomati"><i class="fa-brands fa-github"></i></a>
+        </li>
+    
         
     </ul>
     </div>
@@ -36,6 +56,11 @@ export default {
     </div>
 </template>
 <style scoped lang="scss">
+
+.all-page {
+  width: 100%;
+  height: calc(100vh - 510px);
+}
     .about-us {
   background-color: #f8f8f8;
   padding: 40px;
@@ -81,4 +106,46 @@ export default {
     }
   }
 }
+
+@media (max-width: 768px) {
+    .all-page {
+      height: auto;
+    }
+  }
+
+  @media (max-width: 480px) {
+    .about-us {
+      padding: 20px;
+    }
+    
+    h2 {
+      font-size: 24px;
+    }
+    
+    p {
+      font-size: 16px;
+      margin-bottom: 20px;
+    }
+  }
+
+  @media (max-width: 1400px) {
+    .all-page {
+      height: auto;
+    }
+  }
+
+  @media (max-width: 480px) {
+    .about-us {
+      padding: 20px;
+    }
+    
+    h2 {
+      font-size: 24px;
+    }
+    
+    p {
+      font-size: 16px;
+      margin-bottom: 20px;
+    }
+  }
 </style>
